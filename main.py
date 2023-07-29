6216b865f66d25e16c6f9e759756d98936d28f02
apirrone <awanegenebayo@gmail.com>
2023-07-29 16:23:04 +0200

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from process import process_image, draw_results
 import cv2
 import chromadb
 import json
+import datetime
 
 client = chromadb.PersistentClient(path="pmr")
 collection = client.get_or_create_collection(name="pmr")
@@ -14,18 +15,32 @@ collection = client.get_or_create_collection(name="pmr")
 os.makedirs("screenshots", exist_ok=True)
 sct = mss.mss()
 
+screenshots_metadata = {}
+
+# TODO run process_image in a subprocess (asynchronously)
 i = 0
 while True:
     window_title = utils.get_active_window()
-    im = np.array(sct.grab(sct.monitors[1]))
-    results = process_image(im)
 
+    im = np.array(sct.grab(sct.monitors[1]))
+    cv2.imwrite("screenshots/" + str(i) + ".png", im)
+    t = json.dumps(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    screenshots_metadata[str(i)] = {"source": window_title, "time": t}
+    json.dump(screenshots_metadata, open("screenshots_metadata.json", "w"))
+
+    start = time.time()
+    results = process_image(im)
     collection.add(
         documents=[json.dumps(results)],
-        metadatas=[{"source": window_title}],
+        metadatas=[
+            {
+                "source": window_title,
+                "time": t,
+            }
+        ],
         ids=[str(i)],
     )
+    print("time taken", time.time() - start)
 
-    cv2.imwrite("screenshots/" + str(i) + ".png", im)
     i += 1
     time.sleep(1)
